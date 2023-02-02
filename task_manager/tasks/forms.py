1dbbc0beb94647f06d3fc2d8c3d64db36d3bc3b2
Sachkov Ilya <sos31337@gmail.com>
2023-02-03 02:36:33 +0300

    Add tasks tests

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import TasksModel
 from django.contrib.auth.models import User
+from task_manager.labels.models import LabelModel
 from django.utils.translation import gettext_lazy as _
 
 
 class TaskForm(forms.ModelForm):
-    work_user = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
+    work_user = forms.ModelChoiceField(queryset=User.objects.all(), required=False, label=_('Исполнитель'))
+    labels = forms.ModelMultipleChoiceField(queryset=LabelModel.objects.all(), required=False, label=_('Метки'))
 
     class Meta:
         model = TasksModel
@@ -20,6 +22,4 @@ class TaskForm(forms.ModelForm):
             'name': _('Имя'),
             'description': _('Описание'),
             'statuses': _('Статус'),
-            'work_user': _('Исполнитель'),
-            'labels': _('Метки'),
         }
